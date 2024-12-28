f197e81528d1ab78c8449e313082b0e2c35f14d4
MilkyC@t <92247270+A-MilkyCat@users.noreply.github.com>
2024-12-28 22:23:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactButton = document.getElementById("contactButton");
+
+    contactButton.addEventListener("click", function () {
+        alert("感謝您的聯絡，我們會儘快回覆您！");
+    });
+});
